5aa80a880f7ac3e72b3685bd67a39046338e4577
DavidMaya944 <maya.quiros.david.jesus@gmail.com>
2024-11-20 19:07:29 +0100

    I´ve created the SNK project

---
+++ SNK/SNK/SNKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNKApp.swift
+//  SNK
+//
+//  Created by David Jesús Maya Quirós on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SNKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
