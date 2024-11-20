5d75f427b4bd8ef73fac1ac2f0ebf78a49e41f79
DavidMaya944 <maya.quiros.david.jesus@gmail.com>
2024-11-20 21:24:49 +0100

    I´ve added the transfomers to the app

--- SNK/SNK/SNKApp.swift
+++ SNK/SNK/SNKApp.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 @main
 struct SNKApp: App {
+    init() {
+        ValueTransformer.setValueTransformer(CharactersArrayTransformer(), forName: NSValueTransformerName("CharactersArrayTransformer"))
+        ValueTransformer.setValueTransformer(EpisodesArrayTransformer(), forName: NSValueTransformerName("EpisodesArrayTransformer"))
+        ValueTransformer.setValueTransformer(GroupsArrayTransformer(), forName: NSValueTransformerName("GroupsArrayTransformer"))
+        ValueTransformer.setValueTransformer(InformationTransformer(), forName: NSValueTransformerName("InformationTransformer"))
+        ValueTransformer.setValueTransformer(RelativesArrayTransformer(), forName: NSValueTransformerName("RelativesArrayTransformer"))
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
